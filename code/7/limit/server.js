41d415cd5a04978673d0abb5b396c254e630a114
yuyc <yuyc@cn.fujitsu.com>
2016-08-16 15:07:41 +0800

    原先connectd已不含bodyparser，需要重写该例子

--- code/7/limit/server.js
+++ code/7/limit/server.js
@@ -1,6 +1,11 @@
 var connect = require('connect');
+var bodyParser = require('body-parser');
 
 var app = connect()
-          .use(connect.bodyParser());
+          .use(bodyParser.json())
+          .use(function (req, res) {
+          	res.write("You post:");
+          	res.end(JSON.stringify(req.body));
+          });
 
 app.listen(3000);
