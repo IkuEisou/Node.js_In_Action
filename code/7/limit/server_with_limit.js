31d033f566489bbd32d016a268e9fc5f907a5c81
yuyc <yuyc@cn.fujitsu.com>
2016-08-16 16:49:19 +0800

    由于新版connect移除bodyparser，所以重写实现limit功能

--- code/7/limit/server_with_limit.js
+++ code/7/limit/server_with_limit.js
@@ -1,7 +1,18 @@
 var connect = require('connect');
+var bodyParser = require('body-parser');
+
+function errorHandler(err, req, res, next) {
+	res.setHeader('content-type', 'application/json');
+	res.statusCode = err.status;
+  var msg = "error: " + err.message + "\nerror statusode："+ err.status ;
+
+  console.log(msg);
+  res.setHeader('content-type', 'text/plain');
+	res.end(msg); 
+}
 
 var app = connect()
-          .use(connect.limit('32kb'))
-          .use(connect.bodyParser());
+          .use(bodyParser.json({limit:'1b'}))
+          .use(errorHandler);
 
 app.listen(3000);
