9af74fdbadb80a454e611dd076eb177467994816
yuyc <yuyc@cn.fujitsu.com>
2016-08-17 14:36:37 +0800

    logger不在connect中，改用morgan中间件处理日志

--- code/7/logger.js
+++ code/7/logger.js
@@ -1,5 +1,8 @@
 var connect = require('connect');
+var morgan = require('morgan');
 
 var app = connect()
-          .use(connect.logger())
+          .use(morgan('combined', {
+  			skip: function (req, res) { return res.statusCode < 400 }
+			}))
           .listen(3000);
