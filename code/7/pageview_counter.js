5499d3268c28cb67afc65df8c7dc2dc7e236ee9f
yuyc <yuyc@cn.fujitsu.com>
2016-08-17 14:37:33 +0800

    图标获取中间件更新

--- code/7/pageview_counter.js
+++ code/7/pageview_counter.js
@@ -1,20 +1,21 @@
 var connect = require('connect');
+var favicon = require('serve-favicon');
 
 var app = connect()
-  .use(connect.favicon())
-  .use(connect.cookieParser('keyboard cat'))
-  .use(connect.session())
-  .use(function(req, res, next){
-    var sess = req.session;
-    if (sess.views) {
-      res.setHeader('Content-Type', 'text/html');
-      res.write('<p>views: ' + sess.views + '</p>');
-      res.end();
-      sess.views++;
-    } else {
-      sess.views = 1;
-      res.end('welcome to the session demo. refresh!');
-    }
-  });
+  .use(favicon(__dirname + '/public/favicon.ico'));
+  // .use(connect.cookieParser('keyboard cat'))
+  // .use(connect.session())
+  // .use(function(req, res, next){
+  //   var sess = req.session;
+  //   if (sess.views) {
+  //     res.setHeader('Content-Type', 'text/html');
+  //     res.write('<p>views: ' + sess.views + '</p>');
+  //     res.end();
+  //     sess.views++;
+  //   } else {
+  //     sess.views = 1;
+  //     res.end('welcome to the session demo. refresh!');
+  //   }
+  // });
 
 app.listen(3000);
